8c99373a1dcf48274086a3f24e5aa1aa04886d41
surenpi.com <LinuxSuRen@gmail.com>
2017-02-02 15:03:02 +0800

    增加删除对话框的标签

---
+++ src/main/java/org/suren/autotest/platform/servlet/DialogTag.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.suren.autotest.platform.servlet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.cache.TemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+/**
+ * @author suren
+ * @date 2017年2月2日 下午2:39:31
+ */
+public class DialogTag extends TagSupport
+{
+
+	/**  */
+	private static final long	serialVersionUID	= 1L;
+	
+	private String dialogId;
+
+	@Override
+	public int doStartTag() throws JspException
+	{
+		TemplateLoader templateLoader =
+				new ClassTemplateLoader(this.getClass(), "/template");
+		Configuration configuration = new Configuration();
+		configuration.setTemplateLoader(templateLoader);
+		configuration.setObjectWrapper(new DefaultObjectWrapper()); 
+		configuration.setDefaultEncoding("UTF-8");
+
+		try
+		{
+			ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+			Template template = configuration.getTemplate("dialog.ftl");
+			
+			Map<String, Object> paramMap = new HashMap<String, Object>();
+			paramMap.put("dialogId", dialogId);
+
+			Writer writer = new OutputStreamWriter(byteOut, "UTF-8"); 
+			template.process(paramMap, writer);
+			
+			JspWriter out = this.pageContext.getOut();
+			out.write(new String(byteOut.toByteArray()));
+		}
+		catch (IOException | TemplateException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return super.doStartTag();
+	}
+
+	public String getDialogId()
+	{
+		return dialogId;
+	}
+
+	public void setDialogId(String dialogId)
+	{
+		this.dialogId = dialogId;
+	}
+
+}
---
+++ src/main/resources/template/dialog.ftl
@@ -0,0 +1,27 @@
+<div class="modal fade" id="${dialogId}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+					&times;
+				</button>
+				<h4 class="modal-title">
+					确认
+				</h4>
+			</div>
+			<div class="modal-body">
+				<div class="alert alert-warning">确认删除？</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+				<a class="btn btn-danger btn-ok">确认</a>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal -->
+</div>
+
+<script type="text/javascript">
+$('#${dialogId}').on('show.bs.modal', function(e) {
+	$(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+});
+</script>
