de525248a212e0445bb5d074eadfbd7942e1d7ae
surenpi <zxjlwt@126.com>
2017-02-25 21:26:48 +0800

    增加清空密文的工具方法

--- src/main/java/org/suren/autotest/platform/util/JAXBUtils.java
+++ src/main/java/org/suren/autotest/platform/util/JAXBUtils.java
@@ -9,6 +9,12 @@ import java.io.UnsupportedEncodingException;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
+import org.suren.autotest.platform.schemas.datasource.DataSourcePageFieldType;
+import org.suren.autotest.platform.schemas.datasource.DataSourcePageType;
+import org.suren.autotest.platform.schemas.datasource.DataSources;
+import org.suren.autotest.platform.schemas.datasource.DataSources.DataSource;
+import org.suren.autotest.platform.schemas.datasource.DataTypeEnum;
+
 
 /**
  * @author suren
@@ -16,6 +22,38 @@ import javax.xml.bind.JAXBException;
  */
 public abstract class JAXBUtils
 {
+	public static void clearEncryptData(DataSources dataSources)
+	{
+		if(dataSources == null || dataSources.getDataSource() == null)
+		{
+			return;
+		}
+		
+		for(DataSource dataSource : dataSources.getDataSource())
+		{
+			if(dataSource == null || dataSource.getPage() == null)
+			{
+				continue;
+			}
+			
+			for(DataSourcePageType page : dataSource.getPage())
+			{
+				if(page == null || page.getField() == null)
+				{
+					continue;
+				}
+				
+				for(DataSourcePageFieldType field : page.getField())
+				{
+					if(field.getType() == DataTypeEnum.ENCRYPT)
+					{
+						field.setData("");
+					}
+				}
+			}
+		}
+	}
+	
 	public static <T> T getObj(T clazz, String content)
 	{
 		try
