83e89cab8fec1b6143ddfd135398c9ec81e17035
surenpi <zxjlwt@126.com>
2017-02-25 11:31:14 +0800

    添加常量接口

---
+++ src/main/java/org/suren/autotest/platform/constants/ControllerConstants.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.suren.autotest.platform.constants;
+
+/**
+ * Controller层面使用的常量
+ * @author suren
+ * @date 2017年2月24日 下午4:11:48
+ */
+public interface ControllerConstants
+{
+	/** 用于保存进度信息 */
+	public static final String PROGRESS_PREFIX = "progress_info_";
+	
+	/** 进度信息区分key */
+	public static final String PROGRESS_KEY = "progress_key";
+}
