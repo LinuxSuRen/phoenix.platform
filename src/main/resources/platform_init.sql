5fbf18e6fd31f725ee2c7ed6f24dba4705b50cec
surenpi <zxjlwt@126.com>
2017-03-01 20:09:34 +0800

    新增初始化脚本

---
+++ src/main/resources/platform_init.sql
@@ -0,0 +1,17 @@
+insert into options (
+	id, opt_key, opt_value, remark
+) values (
+	1, 'schema_autotest', 'http://surenpi.com/schema/autotest/autotest.web.framework.xsd', ''
+);
+
+insert into options (
+	id, opt_key, opt_value, remark
+) values (
+	2, 'schema_datasource', 'http://surenpi.com/schema/datasource/autotest.web.framework.datasource.xsd', ''
+);
+
+insert into options (
+	id, opt_key, opt_value, remark
+) values (
+	3, 'schema_suite', 'http://surenpi.com/schema/suite/autotest.web.framework.suite.xsd', ''
+);
