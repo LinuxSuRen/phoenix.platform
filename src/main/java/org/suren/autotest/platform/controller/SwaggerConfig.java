6ccd3455bf2e2542392b5a673bb94fbf6b2818b4
surenpi <zxjlwt@126.com>
2017-06-09 19:14:01 +0800

    增加swagger

---
+++ src/main/java/org/suren/autotest/platform/controller/SwaggerConfig.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.suren.autotest.platform.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author suren
+ * @date 2017年6月9日 上午8:52:35
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket buildDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInf())
+                .select().apis(RequestHandlerSelectors.basePackage("org.suren.autotest.platform.controller"))//controller路径
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo buildApiInf(){
+        return new ApiInfoBuilder()
+                .title("xingguo大标题")
+                .termsOfServiceUrl("http://blog.csdn.net/u014231523网址链接")
+                .description("springmvc swagger2")
+                .contact(new Contact("diaoxingguo", "http://blog.csdn.net/u014231523", "diaoxingguo@163.com"))
+                .build();
+
+    }
+}
